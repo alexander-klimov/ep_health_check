9b6479139d30a27a0554758e30ee691430671afa
Alexander Klimov <alexander.klimov@valamis.com>
2020-02-25 14:01:56 +0300

    Change health endpoint paths
    - /live -> /liveness
    - /ready -> /readiness

--- health.js
+++ health.js
@@ -3,7 +3,7 @@ const health = require('@cloudnative/health-connect');
 let healthcheck = new health.HealthChecker();
 
 exports.expressCreateServer = function(hook, context) {
-    context.app.use('/live', health.LivenessEndpoint(healthcheck));
-    context.app.use('/ready', health.ReadinessEndpoint(healthcheck));
+    context.app.use('/liveness', health.LivenessEndpoint(healthcheck));
+    context.app.use('/readiness', health.ReadinessEndpoint(healthcheck));
     context.app.use('/health', health.HealthEndpoint(healthcheck));
 }
